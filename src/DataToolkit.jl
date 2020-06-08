c8b13f1c1ec37c720f6ec4a348eb9845d7b1f888
Zhiwei Wang <dddarin@icloud.com>
2020-06-09 07:19:18 +0800

    update DataToolkit.jl

--- src/DataToolkit.jl
+++ src/DataToolkit.jl
@@ -7,7 +7,7 @@ using DataFrames
 using Statistics
 using Blink, TableView
 
-export del_special_char, readCSV, unify_city_name, isnumber, allinteger, forceparse, forcedate, @get_namevalue_pairs, winsor!
+export del_special_char, isnumber, notnumber, notdate, parse_m, allinteger, readCSV, unify_city_name, forceparse, forcedate, @get_namevalue_pairs, winsor!
 export @replace!, @drop!, @by!, @filter, @filter!
 
 # export @querying
